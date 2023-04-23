c69236bdc65bcfd8a170647400c8f70a9d1062a2
bixingjian <bixingjian19@163.com>
2023-04-23 16:37:15 -0700

    add bleu score

--- distractor_metric.py
+++ distractor_metric.py
@@ -4,18 +4,35 @@ import re
 from collections import Counter
 from sentence_transformers import SentenceTransformer, util
 import nltk
+from nltk.tokenize import word_tokenize
 
 model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2") #using a relatively smaller size model from the api
 
 # scores for the original distractor
 option_dissim = []
 context_sim = []
+bleu_score = []
 for i in range(10):
     context = context_list[i]
     context_sens = nltk.sent_tokenize(context)
     correct_answer = list(correct_answer_list[i])
     incorrect_answers = incorrect_answer_list[i] # change here for incorrect and distractor
 
+    # bleu score for the original distractor
+    sent_tokenized = word_tokenize(question_list[i])
+    # temp = []
+    # for _ in range(len(incorrect_answer_list[i])):
+    #     answer_tokenized = word_tokenize(incorrect_answers[_])
+    #     temp_score = nltk.translate.bleu_score.sentence_bleu(sent_tokenized, answer_tokenized, weights=(1, 0, 0, 0), smoothing_function=None)
+    #     temp.append(temp_score)
+    # bleu_score.append(temp)
+    # incorrect_answer_tokenized = [word_tokenize(incorrect_answers[_]) for _ in range(len(incorrect_answers))]
+    # answer_score = nltk.translate.bleu_score.sentence_bleu(incorrect_answer_tokenized, sent_tokenized, weights=(1.0, 0, 0, 0), smoothing_function=None)
+    temp = []
+    temp.append(nltk.translate.bleu_score.sentence_bleu(word_tokenize(incorrect_answer_list[i][0]), correct_answer, weights=(1.0, 0, 0, 0)))
+    temp.append(nltk.translate.bleu_score.sentence_bleu(word_tokenize(incorrect_answer_list[i][1]), correct_answer, weights=(1.0, 0, 0, 0)))
+    temp.append(nltk.translate.bleu_score.sentence_bleu(word_tokenize(incorrect_answer_list[i][2]), correct_answer, weights=(1.0, 0, 0, 0)))
+    bleu_score.append(temp)
 
     # Compute encodings for option in_sim
     correct_answer_vectors = model.encode(correct_answer_list[i])
@@ -40,20 +57,27 @@ for i in range(10):
         temp.append(temp_max_score)
     context_sim.append(temp)
 
+
     # print(option_dissim)
     # print(context_sim)
 
-    alpha = 0.3
-    beta = 1 - alpha
 
 # scores for the generated
 gen_option_dissim = []
 gen_context_sim = []
+gen_bleu_score = []
 for i in range(10):
     context = context_list[i]
     context_sens = nltk.sent_tokenize(context)
     correct_answer = list(correct_answer_list[i])
     incorrect_answers = generated_distractor_list[i] # change here for incorrect and distractor
+
+    # compute gen bleu score
+    temp = []
+    temp.append(nltk.translate.bleu_score.sentence_bleu(word_tokenize(generated_distractor_list[i][0]), correct_answer, weights=(1.0, 0, 0, 0)))
+    temp.append(nltk.translate.bleu_score.sentence_bleu(word_tokenize(generated_distractor_list[i][1]), correct_answer, weights=(1.0, 0, 0, 0)))
+    temp.append(nltk.translate.bleu_score.sentence_bleu(word_tokenize(generated_distractor_list[i][2]), correct_answer, weights=(1.0, 0, 0, 0)))
+    gen_bleu_score.append(temp)
 
 
     # Compute encodings for option in_sim
@@ -82,8 +106,13 @@ for i in range(10):
     # print(option_dissim)
     # print(context_sim)
 
-    alpha = 0.3
-    beta = 1 - alpha
+
+alpha = 0.3
+beta = 1 - alpha
+
+print(bleu_score)
+print("+++" *50 )
+print(gen_bleu_score)
 
 
 for i in range(10):
@@ -92,13 +121,15 @@ for i in range(10):
     print("QUESTION: \n", question_list[i], "\n")
     print("CORRECT ANSWER: \n", correct_answer_list[i], "\n")
     for j, (dissim, sim) in enumerate(zip(option_dissim[i],context_sim[i])):
-        # print("distracor ",j, ": ", incorrect_answer_list[i][j], "\n","option_dissim: %.2f |" %float(dissim), "context_sim:  %.2f |" %float(sim), "final score:  %.2f | " %float(alpha * dissim + beta * sim))
-        print(j+1, ": ", incorrect_answer_list[i][j], "\n")
-        # print("*"*10)
+        print("distracor ",j, ": ", incorrect_answer_list[i][j], "\n","option_dissim: %.2f |" %float(dissim), "context_sim:  %.2f |" %float(sim), "final score:  %.2f | " %float(alpha * dissim + beta * sim))
+        print("bleu_score: ", bleu_score[i][j])
+        # print(j+1, ": ", incorrect_answer_list[i][j], "\n")
+        print("*"*10)
     for k, (gen_dissim, gen_sim) in enumerate(zip(gen_option_dissim[i], gen_context_sim[i])):
-        # print("gen_distracor ",k, ": ", generated_distractor_list[i][k], "\n", "gen_option_dissim: %.2f |" %float(gen_dissim), "gen_context_sim:  %.2f |" %float(gen_sim), "gen_final_score:  %.2f | " %float(alpha * gen_dissim + beta * gen_sim))
-        print(k+4, ": ", generated_distractor_list[i][k], "\n")
-        # print("*"*10)
+        print("gen_distracor ",k, ": ", generated_distractor_list[i][k], "\n", "gen_option_dissim: %.2f |" %float(gen_dissim), "gen_context_sim:  %.2f |" %float(gen_sim), "gen_final_score:  %.2f | " %float(alpha * gen_dissim + beta * gen_sim))
+        print("gen_blue_score: ", gen_bleu_score[i][k])
+        # print(k+4, ": ", generated_distractor_list[i][k], "\n")
+        print("*"*10)
     
     
 
